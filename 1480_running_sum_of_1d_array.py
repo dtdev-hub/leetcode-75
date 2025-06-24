807e370ec02baa2a8ec2bb3c35b07fb9696ab6f9
dtdev <54771496+dtdev-hub@users.noreply.github.com>
2025-06-24 13:28:20 +0700

    refactor: 1480

--- 1480_running_sum_of_1d_array.py
+++ 1480_running_sum_of_1d_array.py
@@ -4,26 +4,23 @@ Given an array nums. We define a running sum of an array as runningSum[i] = sum(
 Return the running sum of nums.
 """
 
-class Solution(object):
-    def runningSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        new_list = list()
-        sum = 0
-        for num in nums:
-            sum += num
-            new_list.append(sum)
+from typing import List
 
-        return new_list
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        running_sum = []
+        current_sum = 0
+        for num in nums:
+            current_sum += num
+            running_sum.append(current_sum)
+        return running_sum
 
 # Test Cases
 if __name__ == "__main__":
     solution = Solution()
 
-    assert solution.runningSum([1,2,3,4]) == [1,3,6,10]
-    assert solution.runningSum([1,1,1,1,1]) == [1,2,3,4,5]
-    assert solution.runningSum([3,1,2,10,1]) == [3,4,6,16,17]
+    assert solution.runningSum([1, 2, 3, 4]) == [1, 3, 6, 10]
+    assert solution.runningSum([1, 1, 1, 1, 1]) == [1, 2, 3, 4, 5]
+    assert solution.runningSum([3, 1, 2, 10, 1]) == [3, 4, 6, 16, 17]
 
     print("All tests passed.")
