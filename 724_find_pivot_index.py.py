f57597a6624e0e4919e33364b1e02a753adbd9f4
dtdev <54771496+dtdev-hub@users.noreply.github.com>
2025-06-24 10:31:19 +0700

    chore: improve clarity for 724

--- 724_find_pivot_index.py.py
+++ 724_find_pivot_index.py.py
@@ -17,13 +17,14 @@ class Solution:
         total_sum = sum(nums)
         left_sum = 0
         for i, num in enumerate(nums):
-            if left_sum == total_sum - left_sum - num:
+            right_sum = total_sum - left_sum - num
+            if left_sum == right_sum:
                 return i
             left_sum += num
         return -1
 
 
-# 🧪 Test Cases
+# Test Cases
 if __name__ == "__main__":
     solution = Solution()
 
