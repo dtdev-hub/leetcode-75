7d2d0c36cc449ef9172c5036cceed4a5698d4672
dtdev <54771496+dtdev-hub@users.noreply.github.com>
2025-06-24 11:32:16 +0700

    refactor: clean up logic in 205. Isomorphic Strings

--- 205_isomorphic_strings.py
+++ 205_isomorphic_strings.py
@@ -8,37 +8,32 @@ All occurrences of a character must be replaced with another character while pre
 """
 
 class Solution:
-    def isIsomorphic(self, s: str, t:str) -> bool:
+    def isIsomorphic(self, s: str, t: str) -> bool:
         """
         :type s: str
         :type t: str
         :rtype: bool
         """
-        s_to_t = dict()
-        t_to_s = dict()
-        for sc, tc in zip(s, t):
-            if sc in s_to_t:
-                if s_to_t[sc] != tc:
-                    return False
-            else:
-                s_to_t[sc] = tc
+        if len(s) != len(t):
+            return False
 
-            if tc in t_to_s:
-                if t_to_s[tc] != sc:
-                    return False
-            else:
-                t_to_s[tc] = sc
-
+        s_to_t = {}
+        t_to_s = {}
+        for s_char, t_char in zip(s, t):
+            if (s_char in s_to_t and s_to_t[s_char] != t_char) or \
+               (t_char in t_to_s and t_to_s[t_char] != s_char):
+                return False
+            s_to_t[s_char] = t_char
+            t_to_s[t_char] = s_char
         return True
-
 
 # Test Cases
 if __name__ == "__main__":
     solution = Solution()
 
-    assert solution.isIsomorphic("egg", "add") == True
-    assert solution.isIsomorphic("foo", "bar") == False
-    assert solution.isIsomorphic("paper", "title") == True
-    assert solution.isIsomorphic("badc", "baba") == False
+    assert solution.isIsomorphic("egg", "add") is True
+    assert solution.isIsomorphic("foo", "bar") is False
+    assert solution.isIsomorphic("paper", "title") is True
+    assert solution.isIsomorphic("badc", "baba") is False
 
     print("All tests passed.")
