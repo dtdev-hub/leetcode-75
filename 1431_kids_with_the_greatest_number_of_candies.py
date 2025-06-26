236cc8d547b7282e08b5bb79f02edfc0a62cabe6
dt-japanscript <54771496+dt-japanscript@users.noreply.github.com>
2025-06-26 22:27:47 +0700

    refactor: 1431

--- 1431_kids_with_the_greatest_number_of_candies.py
+++ 1431_kids_with_the_greatest_number_of_candies.py
@@ -11,27 +11,16 @@ from typing import List
 
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
-        max = 0
-        res = []
-        for candy in candies:
-            if candy > max:
-                max = candy
-
-        for candy in candies:
-            if candy + extraCandies >= max:
-                res.append(True)
-            else:
-                res.append(False)
-
-        return res
+        max_candies = max(candies)
+        return [(candy + extraCandies) >= max_candies for candy in candies]
 
 
 # Test Cases
 if __name__ == "__main__":
     solution = Solution()
 
-    assert solution.kidsWithCandies([2,3,5,1,3], 3) == [True, True, True, False, True]
-    assert solution.kidsWithCandies([4,2,1,1,2], 1) == [True, False, False, False, False]
-    assert solution.kidsWithCandies([12,1,12], 10) == [True, False, True]
+    assert solution.kidsWithCandies([2, 3, 5, 1, 3], 3) == [True, True, True, False, True]
+    assert solution.kidsWithCandies([4, 2, 1, 1, 2], 1) == [True, False, False, False, False]
+    assert solution.kidsWithCandies([12, 1, 12], 10) == [True, False, True]
 
     print("All tests passed.")
